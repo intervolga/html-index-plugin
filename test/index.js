7a1145a8ca72555a796cc1ba0a42a8390022e05d
Алексей Шкарупа <shkarupa.alex@gmail.com>
2017-08-17 11:32:35 +0300

    Увеличен лимит времени на прохождение теста

--- test/index.js
+++ test/index.js
@@ -4,14 +4,17 @@ const path = require('path');
 const runWebpack = require('./helpers/run-webpack');
 
 describe('html index plugin', () => {
-  it('should produced expected html', () => {
+  it('should produced expected html', function(done) {
+    this.timeout(30000); // eslint-disable-line no-invalid-this
+
     const entry = path.join(__dirname, 'cases', 'normal', 'source.js');
     const out = path.join(__dirname, 'cases', 'normal', 'index.html');
 
-    return runWebpack(entry).then((result) => {
+    runWebpack(entry).then(function() {
       const producedHtml = fs.readFileSync(out).toString();
       expect(producedHtml).to.contain('page1.html"');
       expect(producedHtml).to.contain('page 1 (produced');
+      done();
     });
   });
 });
